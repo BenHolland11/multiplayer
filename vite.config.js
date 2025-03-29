72bb9c957b4e195b5fa44489133221363adc7a90
BenHolland11 <warzh1114@gmail.com>
2025-03-29 11:30:36 -0500

    d

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Ensures it works on Railway
     port: process.env.PORT || 3000, // You can change this if needed
-    open: true, // Opens the browser automatically
+    open: false, // Opens the browser automatically
+    allowedHosts: ['.railway.app'] // Allow Railway subdomains
   }
 });
